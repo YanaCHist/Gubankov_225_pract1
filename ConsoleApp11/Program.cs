b4bd69857e0f7ee70f2de7f38be77ba517ec873b
Gubankov Stepan <deadhumanjjjjj@vk.com>
2023-11-24 12:49:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //4 Дана длина ребра куба a. Найти объем куба V и площадь его поверхности S.
+            Console.WriteLine("Введите длинну ребра куба:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Обьем куба:" + Math.Pow(a, 3) + " куб.см");
+            Console.WriteLine("Площадь поверхности:" + Math.Pow(a, 2) * 6 + " кв.см");//площать стороны * 6 сторон куба
+            Console.ReadLine();
+        }
+    }
+}
