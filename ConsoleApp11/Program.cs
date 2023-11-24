2b46c7b9dcfb71300e2725a289c7d81876649437
Gubankov Stepan <deadhumanjjjjj@vk.com>
2023-11-24 13:18:11 +0300

    add files

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 
-namespace ConsoleApp11
+namespace Gubankovchick
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double x1, x2, x3, AC, BC;
 
+            Console.Write("Введите координату точки A: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
 
-            //4 Дана длина ребра куба a. Найти объем куба V и площадь его поверхности S.
-            Console.WriteLine("Введите длинну ребра куба:");
-            int a = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Обьем куба:" + Math.Pow(a, 3) + " куб.см");
-            Console.WriteLine("Площадь поверхности:" + Math.Pow(a, 2) * 6 + " кв.см");//площать стороны * 6 сторон куба
+            Console.Write("Введите координату точки B: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите координату точки C: ");
+            x3 = Convert.ToDouble(Console.ReadLine());
+
+            AC = Math.Abs(x3 - x1); // расстояние между точками A и C
+            BC = Math.Abs(x3 - x2); // расстояние между точками B и C
+
+            Console.WriteLine(" AC = " + AC);
+            Console.WriteLine(" BC = " + BC);
+            Console.WriteLine(" AC + BC = " + (AC + BC));
+
             Console.ReadLine();
         }
     }
 }
+
